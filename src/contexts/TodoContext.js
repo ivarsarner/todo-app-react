918c66cf8c871d26ec3d688a96e389a335a35035
ivarsarner <ivar.sarner@appliedtechnology.se>
2020-04-08 12:13:45 +0200

    fixed toggle

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -4,38 +4,38 @@ import uuid from 'uuid/v4';
 export const TodoContext = createContext();
 
 const TodoContextProvider = (props) => {
-	const [todos, setTodos] = useState(() => {
-		const localData = localStorage.getItem('todos');
-		return localData ? JSON.parse(localData) : [];
-	});
+  const [todos, setTodos] = useState(() => {
+    const localData = localStorage.getItem('todos');
+    return localData ? JSON.parse(localData) : [];
+  });
 
-	const addTodo = (title, desc) => {
-		const newTodo = { id: uuid(), title, desc, done: false };
-		setTodos([...todos, newTodo]);
-	};
+  const addTodo = (title, desc) => {
+    const newTodo = { id: uuid(), title, desc, done: false };
+    setTodos([...todos, newTodo]);
+  };
 
-	const removeTodo = (id) => {
-		setTodos(todos.filter((todo) => todo.id !== id));
-	};
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-	const toggleDone = (id) => {
-		const todo = todos.find((todo) => todo.id === id);
-		console.log(todo);
-		console.log(todos);
-		//setTodos();
-	};
+  const toggleDone = (id) => {
+    const toggledTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(toggledTodos);
+  };
 
-	useEffect(() => {
-		localStorage.setItem('todos', JSON.stringify(todos));
-	}, [todos]);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
-	return (
-		<TodoContext.Provider
-			value={{ todos, todosActions: { addTodo, removeTodo, toggleDone } }}
-		>
-			{props.children}
-		</TodoContext.Provider>
-	);
+  return (
+    <TodoContext.Provider
+      value={{ todos, todosActions: { addTodo, removeTodo, toggleDone } }}
+    >
+      {props.children}
+    </TodoContext.Provider>
+  );
 };
 
 export default TodoContextProvider;
